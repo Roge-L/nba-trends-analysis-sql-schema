17935ac1f0551c8dd0fb28f235afdd2dccd170a1
Roge-L <roger.l.jiahao2@gmail.com>
2021-11-18 00:11:29 -0500

    Fiddle with nba_api

--- data.py
+++ data.py
@@ -1,12 +1,20 @@
 import requests
 import json
+from nba_api.stats.endpoints import cumestatsplayer
+from nba_api.stats.static import players, teams
 
-# https://github.com/swar/nba_api/blob/master/docs/nba_api/stats/endpoints/shotchartdetail.md [SHOT]
+players_dict = players.get_players()
+team_dict = teams.get_teams()
+
 # https://github.com/swar/nba_api/blob/master/docs/nba_api/stats/endpoints/cumestatsplayer.md [PLAYER]
 # https://github.com/swar/nba_api/blob/master/docs/nba_api/stats/endpoints/cumestatsteam.md [TEAM]
+# https://github.com/swar/nba_api/blob/master/docs/nba_api/stats/endpoints/shotchartdetail.md [SHOT]
 # https://github.com/swar/nba_api/blob/master/docs/nba_api/stats/endpoints/boxscoresummaryv2.md [GAME]
 
-player = requests.get()
-team = requests.get()
-shot = requests.get()
-game = requests.get()
+player = cumestatsplayer.CumeStatsPlayer(GameIDs='21700807', PlayerID='2544', LeagueID='00', Season='2019-20', SeasonType='Regular+Season')
+# player = requests.get()
+# team = requests.get()
+# shot = requests.get()
+# game = requests.get()
+# print(players_dict)
+print(player)
