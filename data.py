d756c2a2fcc0a9a97ff0422b87af8002f693958e
Wayne Zhu <way87767@gmail.com>
2021-11-19 18:41:40 -0500

    remove gleague-nba games

--- data.py
+++ data.py
@@ -167,18 +167,56 @@ class NBA:
     games = res.get_data_frames()[0].to_dict(orient='records')
     data = []
 
+    team_ids = set([
+      1610612737,
+      1610612738,
+      1610612739,
+      1610612740,
+      1610612741,
+      1610612742,
+      1610612743,
+      1610612744,
+      1610612745,
+      1610612746,
+      1610612747,
+      1610612748,
+      1610612749,
+      1610612750,
+      1610612751,
+      1610612752,
+      1610612753,
+      1610612754,
+      1610612755,
+      1610612756,
+      1610612757,
+      1610612758,
+      1610612759,
+      1610612760,
+      1610612761,
+      1610612762,
+      1610612763,
+      1610612764,
+      1610612765,
+      1610612766
+    ])
+
     home_games = {}
     away_games = {}
 
     for game in games:
       isAway = '@' in game['MATCHUP']
       id = game['GAME_ID']
+      if game['TEAM_ID'] not in team_ids:
+        continue
+
       if not isAway:
         home_games[id] = game
       else:
         away_games[id] = game
 
     for key in list(home_games.keys()):
+      if key not in home_games or key not in away_games:
+        continue
       home = home_games[key]
       away = away_games[key]
       dct = {
@@ -222,5 +260,5 @@ def generate_all():
 
 # will take about 2 hours!
 generate_all()
-# t = TeamDetails(team_id=1612709909)
-# print(t.get_data_frames())
+# t = TeamDetails(team_id=1610616833)
+# print(t.get_dict())
